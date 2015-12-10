cc096b255c294ff5c26b058108b7298fa4458eb1
Simon Rozet <me@simonrozet.com>
2015-12-10 15:54:35 +0000

    newline fixes

--- gcloud/gcloud.go
+++ gcloud/gcloud.go
@@ -1,4 +1,5 @@
 // Package gcloud integrates protolog with the Google Cloud Logging service.
+//
 // See https://cloud.google.com/logging/docs/ for more details.
 package gcloud
 
@@ -7,8 +8,7 @@ import (
 	"google.golang.org/api/logging/v1beta3"
 )
 
-// NewPusher creates a new protolog.Pusher that logs using the
-// Google Cloud Logging API.
+// NewPusher creates a new protolog.Pusher that logs using the Google Cloud Logging API.
 func NewPusher(
 	service *logging.ProjectsLogsEntriesService,
 	projectID string,
